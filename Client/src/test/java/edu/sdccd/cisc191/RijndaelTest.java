6fb3ef45dcf56a5aa7b2e9c3abdb52fef29bb241
Oliver Tran <olivertran114@gmail.com>
2024-04-07 23:13:30 -0700

    Finished AES Encryption (ECB)

--- Client/src/test/java/edu/sdccd/cisc191/RijndaelTest.java
+++ Client/src/test/java/edu/sdccd/cisc191/RijndaelTest.java
@@ -12,5 +12,6 @@ public class RijndaelTest {
         String inputText = "32 43 f6 a8 88 5a 30 8d 31 31 98 a2 e0 37 07 34";
         String cipherKey = "2b 7e 15 16 28 ae d2 a6 ab f7 15 88 09 cf 4f 3c";
         Rijndael aes = new Rijndael(inputText, cipherKey);
+        aes.encode();
     }
 }
