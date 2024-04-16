f86d981c1a2934391f4dc55263fd32bd7726db80
Oliver Tran <olivertran114@gmail.com>
2024-04-16 00:08:00 -0700

    Finished AES Encryption and Decryption with CTR and ECB (missing CTR random IV)

--- Client/src/test/java/edu/sdccd/cisc191/SHA256Test.java
+++ Client/src/test/java/edu/sdccd/cisc191/SHA256Test.java
@@ -1,18 +1,20 @@
 package edu.sdccd.cisc191;
 
-import edu.sdccd.cisc191.hashes.SHA;
-import org.junit.jupiter.api.Assertions;
+import edu.sdccd.cisc191.hashes.SHA2;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SHA256Test {
-
-    private String inputText;
-
     @Test
     void testBasicHash() {
-        inputText = "abc";
-        SHA sha = new SHA(inputText);
+        SHA2 sha2 = new SHA2("abc");
+        assertEquals("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad", sha2.runHash());
+    }
+
+    @Test
+    void testLargeHash() {
+        SHA2 sha2 = new SHA2("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+        assertEquals("248d6a61d20638b8e5c02693c3e6039a33ce45964ff2167f6ecedd419db06c1", sha2.runHash());
     }
 }
