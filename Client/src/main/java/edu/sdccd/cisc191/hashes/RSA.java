a6c8738dcc706cb906dc83ab17261c063a68460a
Oliver Tran <olivertran114@gmail.com>
2024-04-16 12:05:37 -0700

    Initial Commit

---
+++ Client/src/main/java/edu/sdccd/cisc191/hashes/RSA.java
@@ -0,0 +1,5 @@
+package edu.sdccd.cisc191.hashes;
+
+public class RSA {
+
+}
---
+++ Client/src/test/java/edu/sdccd/cisc191/RSATest.java
@@ -0,0 +1,18 @@
+package edu.sdccd.cisc191;
+
+import edu.sdccd.cisc191.hashes.RSA;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RSATest {
+
+    private String plainText, cipherText;
+    private String key;
+
+    @Test
+    void encode() {
+
+    }
+}
