ef11cc5987130baebb596b9bf37a100752aa7617
Oliver Tran <olivertran114@gmail.com>
2024-04-16 21:54:48 -0700

    Finished RSA encryption and decryption (without javafx)

--- Client/src/main/java/edu/sdccd/cisc191/hashes/RSA.java
+++
@@ -1,5 +0,0 @@
-package edu.sdccd.cisc191.hashes;
-
-public class RSA {
-
-}
--- Client/src/test/java/edu/sdccd/cisc191/RSATest.java
+++ Client/src/test/java/edu/sdccd/cisc191/RSATest.java
@@ -1,18 +1,25 @@
 package edu.sdccd.cisc191;
 
-import edu.sdccd.cisc191.hashes.RSA;
-import org.junit.jupiter.api.Assertions;
+import edu.sdccd.cisc191.ciphers.RSA;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RSATest {
 
-    private String plainText, cipherText;
-    private String key;
+    private BigInteger inputText, cipherText;
 
     @Test
     void encode() {
+        RSA alice = new RSA(2048);
+        System.out.println("Private Key:\np: " + alice.getSecretPrimes()[0].toString(16) + ",\nq: " + alice.getSecretPrimes()[1].toString(16));
+        System.out.println("\nPublic Key:\nn: " + alice.getEncryptionKey()[0].toString(16) + ",\ne: " + alice.getEncryptionKey()[1].toString(16));
 
+        inputText = new BigInteger("000102030405060708090a0b0c0d0e0f", 16);
+
+        cipherText = RSA.encode(inputText, alice.getEncryptionKey());
+        assertEquals(inputText.toString(16), alice.decode(cipherText).toString(16));
     }
 }
