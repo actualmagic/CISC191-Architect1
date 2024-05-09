bc3a6a273b33e4cb1b0f48c1d7b8a42a216789eb
vtong06 <vinhtong1234@gmail.com>
2024-05-09 14:37:51 -0700

    hotfix

--- Architect/src/test/java/edu/sdccd/cisc191/EnigmaTest.java
+++ Architect/src/test/java/edu/sdccd/cisc191/EnigmaTest.java
@@ -5,6 +5,8 @@ import edu.sdccd.cisc191.ciphers.EnigmaEngine;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -31,14 +33,14 @@ public class EnigmaTest {
     }
 
     @Test
-    public void testEnigmaCryptanalysis() {
+    public void testEnigmaCryptanalysis() throws SQLException {
         String inputText = "I PROPOSE to consider the question, ‘Can machines think?’ This should begin with definitions of the meaning of the terms ‘machine’ and ‘think’. The definitions might be framed so as to reflect so far as possible the normal use of the words, but this attitude is dangerous. If the meaning of the words ‘machine’ and ‘think’ are to be found by examining how they are commonly used it is difficult to escape the conclusion that the meaning and the answer to the question, ‘Can machines think?’ is to be sought in a statistical survey such as a Gallup poll. But this is absurd. Instead of attempting such a definition I shall replace the question by another, which is closely related to it and is expressed in relatively unambiguous words.";
         String plainText = inputText.toUpperCase().replaceAll("[^A-Z]", "");
 
         Enigma enigma = new Enigma(new int[]{5,5,1}, new int[]{3,10,2}, new int[]{1,15,3}, "UKW B", "AB CD EF GH IJ KL MN");
         String cipherText = enigma.encode(plainText);
 
-        EnigmaEngine engine = new EnigmaEngine(cipherText, 32);
+        EnigmaEngine engine = new EnigmaEngine(cipherText, 16);
         String output = engine.cryptanalyze();
 
         assertEquals(plainText, output);
